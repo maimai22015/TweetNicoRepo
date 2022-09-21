bffa0190af960bac89810cc0e257d5cc663d41c8
maimai <maimai2_2015@yahoo.co.jp>
2022-09-22 00:40:33 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+CONFIG = {"CONSUMER_KEY": "xxx", # Replace Your API Key
+     "CONSUMER_SECRET": "xxxxxxxxxxxx", 
+     "ACCESS_TOKEN": "xxxxxxxxxxxxxxxxxxxxxxxx", 
+     "ACCESS_SECRET": "xxxxxxxxxxxxxxxxxxxxxx",
+     "BEARER_TOKEN" : "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+     }
